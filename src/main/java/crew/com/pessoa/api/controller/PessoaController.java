d8bba39ed84d07fb60fafc108dd6e99fee6edf4e
Marcelo <marcelovtneves@gmail.com>
2023-01-12 13:34:56 -0300

    feito a controller de pessoa

--- src/main/java/crew/com/pessoa/api/controller/PessoaController.java
+++ src/main/java/crew/com/pessoa/api/controller/PessoaController.java
@@ -1,5 +1,6 @@
 package crew.com.pessoa.api.controller;
 
+import com.electronwill.nightconfig.core.conversion.Path;
 import crew.com.pessoa.domain.entity.Pessoa;
 import crew.com.pessoa.domain.service.PessoaService;
 import lombok.RequiredArgsConstructor;
@@ -8,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @RestController
@@ -30,4 +32,25 @@ public class PessoaController {
         return ResponseEntity.status(HttpStatus.OK).body(pessoas);
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<Pessoa> findById(@PathVariable Long id) {
+        Optional<Pessoa> optPessoa = pessoaService.findById(id);
+        if(optPessoa.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+        Pessoa pessoa = optPessoa.get();
+        return ResponseEntity.status(HttpStatus.OK).body(pessoa);
+    }
+
+    @PutMapping
+    public ResponseEntity<Pessoa> update(@RequestBody Pessoa pessoa) {
+        Pessoa pessoaSaved = pessoaService.save(pessoa);
+        return ResponseEntity.status(HttpStatus.OK).body(pessoaSaved);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteById(@PathVariable Long id) {
+        pessoaService.deleteById(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
 }
