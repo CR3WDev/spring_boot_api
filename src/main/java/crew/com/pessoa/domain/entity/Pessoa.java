9850fab25b6ea31d4f1b47c00133c0e78b440fc3
Marcelo <marcelovtneves@gmail.com>
2023-01-13 16:59:55 -0300

    resolvido erro no projeto

--- src/main/java/crew/com/pessoa/domain/entity/Pessoa.java
+++ src/main/java/crew/com/pessoa/domain/entity/Pessoa.java
@@ -1,12 +1,15 @@
 package crew.com.pessoa.domain.entity;
 
+import crew.com.pessoa.api.response.EnderecoResponse;
 import jakarta.persistence.*;
-import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.With;
 
 import java.time.LocalDateTime;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Getter
 @Setter
@@ -19,6 +22,4 @@ public class Pessoa {
     private String nome;
     private LocalDateTime data_de_nascimento;
     private Long endereco_principal_id;
-
-    private List<Endereco> enderecos;
 }
